cdf8893eab898e10214a95a77823a8a572f32537
Simon Langhoff <simlanghoff@gmail.com>
2015-04-15 21:53:19 +0200

    Now it actually reads the image from the meta box (movie poster)

--- wp-content/themes/itufilm/single.php
+++ wp-content/themes/itufilm/single.php
@@ -1,5 +1,8 @@
+SINGLE PAGE
+
 <?php get_header(); ?>
 <section id="content" role="main">
+    THIS IS THE SINGLE PAGE
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
